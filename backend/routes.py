f9d94de419f6cd14f040d9bbecad2c1aa7451574
Ehbraheem <ehbraheem@gmail.com>
2024-10-09 06:29:00 -0400

    implemented pictures service

--- backend/routes.py
+++ backend/routes.py
@@ -7,6 +7,8 @@ SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 json_url = os.path.join(SITE_ROOT, "data", "pictures.json")
 data: list = json.load(open(json_url))
 
+find_picture_by_id = lambda id: next((pic for pic in data if pic['id'] == id), None)
+
 ######################################################################
 # RETURN HEALTH OF THE APP
 ######################################################################
@@ -35,7 +37,7 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    return jsonify(data), 200
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +46,9 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    res = find_picture_by_id(id)
+    status_code = 200 if res else 404
+    return jsonify(res), status_code
 
 
 ######################################################################
@@ -52,7 +56,18 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    picture = request.json
+    if not picture:
+        return jsonify(Message="Invalid input parameter"), 422
+    elif find_picture_by_id(picture['id']):
+        return jsonify(Message=f"picture with id {picture['id']} already present"), 302
+    
+    try:
+        data.append(picture)
+    except Exception:
+        return {"Message": "data not defined"}, 500
+
+    return jsonify(picture), 201
 
 ######################################################################
 # UPDATE A PICTURE
@@ -61,11 +76,37 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    payload = request.json
+    
+    if not payload:
+        return jsonify(Message="Invalid input parameter"), 422
+
+    picture = find_picture_by_id(id)
+    
+    if not picture:
+        return jsonify(Message="picture not found"), 404
+    
+    try:
+        for k,v in payload.items():
+            picture[k] = v
+        
+    except Exception:
+        return {"Message": "data not defined"}, 500
+
+    return jsonify(picture), 200
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    picture = find_picture_by_id(id)
+    if not picture:
+        return jsonify(Message="picture not found"), 404
+    
+    try:
+        data.remove(picture)
+    except Exception:
+        return {"Message": "data not defined"}, 500
+
+    return jsonify(picture), 204
